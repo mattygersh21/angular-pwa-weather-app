e7b0f2d04e073edf64c23d157e01c7ae4fe68772
mattygersh21 <dakotaseia@hotmail.com>
2024-02-24 16:06:22 -0600

    * Added Weather and WeatherInfo interfaces/models.

---
+++ weather-app/src/app/models/weather.ts
@@ -0,0 +1,20 @@
+interface WeatherInfo {
+    main: string;
+    icon: string;
+}
+
+export interface Weather {
+    weather: WeatherInfo[];
+    main: {
+        temp: number;
+        pressure: number;
+        humidity: number;
+    };
+    wind: {
+        speed: number;
+    };
+    sys: {
+        country: string;
+    }
+    name: string;
+}
